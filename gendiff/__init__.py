9f74eb79905063cf9f3ae9e71bd6c64cd563b76a
Anna Bystrova <annbystr@gmail.com>
2023-03-10 13:17:20 +0300

    add henerate_diff

---
+++ gendiff/__init__.py
@@ -0,0 +1,5 @@
+from gendiff.modules.generate_diff import generate_diff
+
+__all__pip = (
+    'generate_diff',
+)
---
+++ gendiff/modules/generate_diff.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+
+def generate_diff(path_data_1, path_data_2):
+    path_1 = os.path.abspath(path_data_1)
+    path_2 = os.path.abspath(path_data_2)
+    data_1 = json.load(open(path_1))
+    data_2 = json.load(open(path_2))
+    keys = sorted(data_1.keys() | data_2.keys())
+    answer = {}
+    for key in keys:
+        if key not in data_1:
+            answer["+ " + key] = data_2[key]
+        elif key not in data_2:
+            answer["- " + key] = data_1[key]
+        elif data_1[key] == data_2[key]:
+            answer["  " + key] = data_1[key]
+        else:
+            answer["- " + key] = data_1[key]
+            answer["+ " + key] = data_2[key]
+    return json.dumps(answer, indent=4).replace('"', '')
